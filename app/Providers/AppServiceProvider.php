491f7978d91273ecbacc7ba0bb3f57ded1554926
Alonso <alonsopv0@gmail.com>
2026-04-16 01:30:25 -0500

    fix: importar Artisan para storage link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,27 +5,24 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Paginator::useBootstrapFive();
 
         URL::forceScheme('https');
 
-if (!file_exists(public_path('storage'))) {
-        Artisan::call('storage:link');
+ 
+        if (!file_exists(public_path('storage'))) {
+            Artisan::call('storage:link');
+        }
     }
 }
